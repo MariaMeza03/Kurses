e22d0f40745e1792817436985f3980aac803a2f4
Maria Meza <mmeza@licorne.mx>
2023-05-12 07:27:40 -0600

    ...

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,18 @@
 const header = document.querySelector('.header')
 
 //*Menu
-const toggle = document.querySelector('.header__mobile__buttons__toggle')
+const links = document.querySelector('.menu__links')
+const toggle = document.querySelector('.header__mobile__toggle')
 const menu = document.querySelector('.menu')
 
-//*idioms-Responsive
-const idiomButton = document.querySelector('.header__mobile__buttons__idiom')
-const idiom = document.querySelector('.idiom')
-
-//*idioms-Desktop
-const idiomButtonDes = document.querySelector('.header__desktop__right__idiom')
-const idiomDes = document.querySelector('.idiom')
 
 function toggleMenu() {
 	menu.classList.toggle('menu--active')
-	toggle.classList.toggle('header__mobile__buttons__toggle--active')
 }
 
-function toggleIdiom() {
-	idiom.classList.toggle('idiom--active')
-}
-
-function toggleIdiomDes() {
-	idiomDes.classList.toggle('idiom--active')
+function DeleteMenu() {
+	menu.classList.remove('menu--active')
 }
 
 toggle.addEventListener('click', toggleMenu)
-idiomButton.addEventListener('click', toggleIdiom)
-idiomButtonDes.addEventListener('click', toggleIdiomDes)
+links.addEventListener('click' ,DeleteMenu)
