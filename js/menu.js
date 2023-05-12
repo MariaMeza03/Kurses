7fb5289c76cdbc75ec32dad4927deac87d408335
Maria Meza <mmeza@licorne.mx>
2023-05-12 09:12:33 -0600

    ...

--- js/menu.js
+++ js/menu.js
@@ -13,6 +13,14 @@ function toggleMenu() {
 function DeleteMenu() {
 	menu.classList.remove('menu--active')
 }
+window.onscroll = function(){
+	const scrollY = window.scrollY
+	const menuActive = document.querySelector('.menu--active')
+
+	scrollY > 100 && menuActive === null ? header.classList.add('header--hide') : header.classList.remove('header--hide')
+	scrollY < 100 && menuActive !== '' ? header.classList.remove('header--show') : false
+
+}
 
 toggle.addEventListener('click', toggleMenu)
 links.addEventListener('click' ,DeleteMenu)
